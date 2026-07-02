a941ac6f3e1e430dad04a3ef048c9e856bfe1a19
Pierre1233424 <pierreeichenauer@icloud.com>
2026-07-02 02:21:24 +0200

    Add dynamic export to layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 const navItems = [
   { href: "/calendar", label: "Kalender" },
   { href: "/customers", label: "Kunden" },
